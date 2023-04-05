df6c443f60069f62e527bccb1097a85a1e1ecf49
moatazzanwarr <moatazzanwarr@gmail.com>
2023-04-05 15:29:21 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 var prAside = document.getElementById("pr-aside");
 var aside = document.getElementById("aside");
 var main = document.querySelector("main");
+var shorts = document.getElementById("shorts");
 
 function openn(){
     prAside.style.width = "100%";
     aside.style.width = "200px";
     main.style.marginRight = "100px";
+    shorts.style.marginRight = "100px";
+    
 }
 
 function closee(){
     prAside.style.width = "0px";
     aside.style.width = "0px";
     main.style.marginRight = "0px";
+    shorts.style.marginRight = "0px";
 }
